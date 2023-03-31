be010c6ffd2248052665df27e2376a900fd2b4b3
syipmong <syipmong5@gmail.com>
2023-03-30 21:27:46 -0700

    m

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *_strcat- concats 2 strings
+ *@dest: char
+ *@src: char;
+ *Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int length = 0;
+while (*dest != '\0')
+{
+	dest++;
+	length++;
+}
+while (*src)
+{
+*dest = *src;
+dest++;
+src++;
+length++;
+
+}
+*(dest + 1) = '\0';
+dest = dest - length;
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+
+/**
+ * File: main.h
+ * Auth: yipmong said
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int _putchar(char c);
