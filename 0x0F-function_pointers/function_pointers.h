4482e8fa9d1433b43fc337e44ebd748b01f5d6c8
syipmong <syipmong5@gmail.com>
2023-04-19 21:09:30 -0700

    filed updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUN_H
 #define FUN_H
 #include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
