866a525521d805fd48a2033f70a8836d0b5c718b
syipmong <syipmong5@gmail.com>
2023-04-20 20:48:58 -0700

    m

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,28 +1,12 @@
-#ifndef VARIADIC
-#define VARIADIC
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+
+#include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif
-
-
-#ifndef PRINT
-#define PRINT
-
-#include <stdarg.h>
-/**
- * struct print_function - mapping format with function
- *
- * @format: char * input
- * @function: a pointer to function
- *
- */
-typedef struct print_function
-{
-	char *format;
-	void (*function)(va_list args);
-} print_function;
 
 #endif
