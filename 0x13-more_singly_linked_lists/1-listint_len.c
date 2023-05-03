f80bb4ec25de4abde10e6fbb77ea520dca552314
syipmong <syipmong5@gmail.com>
2023-05-02 18:05:52 -0700

    file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t.
+ *
+ * @h: const listint_t input
+ *
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
